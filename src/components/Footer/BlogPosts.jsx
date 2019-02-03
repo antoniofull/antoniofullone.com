fbec46fbece20b6afee5071e9423560f3e37d161
Antonio <antonio.fullone@gmail.com>
2019-02-03 11:07:11 +0100

    a better grid on tablets
    Former-commit-id: 2caf2a7ad7c5c0d891a0fa16c97d7275a598dfd5

--- src/components/Footer/BlogPosts.jsx
+++ src/components/Footer/BlogPosts.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import { blogData } from '../../data';
 
 const BlogPosts = () => (
-  <section className="blog">
-    <h3 className="footer-header">Latest from the Blog</h3>
-    <div className="post-container">
-      {blogData.length > 0 &&
-        blogData.map(item => (
-          <article key={item.id} className="post">
-            <h3 className="post__title text-white">{item.title}</h3>
-            <a href={item.link} className="post__link">
-              <img className="post__image" src={item.img} alt={item.title} />
-            </a>
-          </article>
-        ))}
-    </div>
+  <section className="blog grid">
+    <h3 className="footer-header footer-header--full grid__item--full-width">
+      Latest from the Blog
+    </h3>
+    {blogData.length > 0 &&
+      blogData.map(item => (
+        <article key={item.id} className="post">
+          <h3 className="post__title text-white">{item.title}</h3>
+          <a href={item.link} className="post__link">
+            <img className="post__image" src={item.img} alt={item.title} />
+          </a>
+        </article>
+      ))}
   </section>
 );
 
