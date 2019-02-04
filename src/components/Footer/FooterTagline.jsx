36fbd13bd7e59f23e6c4ed4c165b35aa3e551638
Antonio <antonio.fullone@gmail.com>
2019-02-04 21:24:01 +0100

    footer ready, wop!
    Former-commit-id: 7098139c241b41576a53e0e352d72ffa0a96f0a8

--- src/components/Footer/FooterTagline.jsx
+++ src/components/Footer/FooterTagline.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FooterTagline = () => (
-  <div className="footer-tagline ">
+  <div className="footer-tagline">
     <p className="freight-sans">
       This website is built with React Js and Netlify. The Blog uses Jekyll as
       CMS with a custom theme. The source code is hosted on Github. The Domain
