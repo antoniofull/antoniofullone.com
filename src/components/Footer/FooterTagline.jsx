40ef6836fed388551a0c490f5397082fc5625c57
Antonio <antonio.fullone@gmail.com>
2019-02-04 20:07:25 +0100

    footer on desktop and large screen
    Former-commit-id: d65368e256e220bc2db854f087803000b80f8660

--- src/components/Footer/FooterTagline.jsx
+++ src/components/Footer/FooterTagline.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const FooterTagline = () => (
-  <p className="footer-tagline freight-sans">
-    This website is built with React Js and Netlify. The Blog uses Jekyll as CMS
-    with a custom theme. The source code is hosted on Github. The Domain is
-    hosted by Hostgator.
-  </p>
+  <div className="footer-tagline ">
+    <p className="freight-sans">
+      This website is built with React Js and Netlify. The Blog uses Jekyll as
+      CMS with a custom theme. The source code is hosted on Github. The Domain
+      is hosted by Hostgator.
+    </p>
+  </div>
 );
 
 export default FooterTagline;
