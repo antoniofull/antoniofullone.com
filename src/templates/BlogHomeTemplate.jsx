17f14756f070f29b3409db7eb23982a07e6132ea
Antonio <antonio.fullone@gmail.com>
2019-03-02 18:48:34 +0100

    second row: check
    Former-commit-id: 506a29679bd456aacbd3dd34dc8e545e991d028c

--- src/templates/BlogHomeTemplate.jsx
+++ src/templates/BlogHomeTemplate.jsx
@@ -49,74 +49,208 @@ class BlogHomeTemplate extends Component {
             <article
               className={classnames('blogpost', {
                 'blogpost--first': index === 0,
-                'second-row': index > 0 && index < 4,
-                'third-row': index > 3
+                'second-row': index > 0 && index < 5,
+                'second-row--odd': index === 1 || index === 3,
+                'single-row': index > 4
               })}
               key={post.node.id}
             >
               <MediaQuery query="(min-device-width: 1024px)">
-                {post.node.frontmatter.image && (
-                  <img
-                    className="post-image"
-                    src={post.node.frontmatter.image}
-                    alt=""
-                  />
-                )}
-                <div className="post-content">
-                  <header className="post-header">
-                    {index <= 0 ? (
-                      <h1
-                        className={classnames(
-                          'post-title',
-                          {
-                            'bodoni-24': index === 0
-                          },
-                          {
-                            'freight-sans': index > 0
-                          }
-                        )}
-                      >
-                        {post.node.frontmatter.title}
-                      </h1>
-                    ) : (
-                      <h2 className="post-title">
-                        {post.node.frontmatter.title}
-                      </h2>
+                {index === 0 && (
+                  <React.Fragment>
+                    {post.node.frontmatter.image && (
+                      <img
+                        className="post-image"
+                        src={post.node.frontmatter.image}
+                        alt=""
+                      />
                     )}
-                    <time
-                      className="post-date"
-                      dateTime={new Date(
-                        post.node.frontmatter.date
-                      ).toLocaleString('en-US', {
-                        timeZone: 'UTC'
-                      })}
-                    >
-                      {new Date(post.node.frontmatter.date).toLocaleString(
-                        'en-US',
-                        {
-                          timeZone: 'UTC'
-                        }
+                    <div className="post-content">
+                      <header className="post-header">
+                        {index <= 0 ? (
+                          <h1
+                            className={classnames(
+                              'post-title',
+                              {
+                                'bodoni-24': index === 0
+                              },
+                              {
+                                'freight-sans': index > 0
+                              }
+                            )}
+                          >
+                            {post.node.frontmatter.title}
+                          </h1>
+                        ) : (
+                          <h2 className="post-title">
+                            {post.node.frontmatter.title}
+                          </h2>
+                        )}
+                        <time
+                          className="post-date"
+                          dateTime={new Date(
+                            post.node.frontmatter.date
+                          ).toLocaleString('en-US', {
+                            timeZone: 'UTC'
+                          })}
+                        >
+                          {new Date(post.node.frontmatter.date).toLocaleString(
+                            'en-US',
+                            {
+                              timeZone: 'UTC'
+                            }
+                          )}
+                        </time>
+                      </header>
+                      {post.node.frontmatter.introduction && (
+                        <p className="post-excerpt">
+                          {`${striptags(posts[0].node.html)
+                            .split(' ')
+                            .slice(0, index === 0 ? this.state.textLines : 40)
+                            .join(' ')} ...`}
+                        </p>
                       )}
-                    </time>
-                  </header>
-                  {post.node.frontmatter.introduction && (
-                    <p className="post-excerpt">
-                      {`${striptags(posts[0].node.html)
-                        .split(' ')
-                        .slice(0, index === 0 ? this.state.textLines : 40)
-                        .join(' ')} ...`}
-                    </p>
-                  )}
-                  <Link
-                    className={`read-more`}
-                    key={post.node.id}
-                    to={post.node.frontmatter.path}
-                  >
-                    Read Post
-                  </Link>
-                </div>
+                      <Link
+                        className={`read-more`}
+                        key={post.node.id}
+                        to={post.node.frontmatter.path}
+                      >
+                        Read Post
+                      </Link>
+                    </div>
+                  </React.Fragment>
+                )}
+                {index > 0 && index < 5 && (
+                  <React.Fragment>
+                    <div className="post-content">
+                      <header className="post-header">
+                        {index <= 0 ? (
+                          <h1
+                            className={classnames(
+                              'post-title',
+                              {
+                                'bodoni-24': index === 0
+                              },
+                              {
+                                'freight-sans': index > 0
+                              }
+                            )}
+                          >
+                            {post.node.frontmatter.title}
+                          </h1>
+                        ) : (
+                          <h2 className="post-title">
+                            {post.node.frontmatter.title}
+                          </h2>
+                        )}
+                        <time
+                          className="post-date"
+                          dateTime={new Date(
+                            post.node.frontmatter.date
+                          ).toLocaleString('en-US', {
+                            timeZone: 'UTC'
+                          })}
+                        >
+                          {new Date(post.node.frontmatter.date).toLocaleString(
+                            'en-US',
+                            {
+                              timeZone: 'UTC'
+                            }
+                          )}
+                        </time>
+                      </header>
+                      {post.node.frontmatter.image && (
+                        <img
+                          className="post-image"
+                          src={post.node.frontmatter.image}
+                          alt=""
+                        />
+                      )}
+
+                      {post.node.frontmatter.introduction && (
+                        <p className="post-excerpt">
+                          {`${striptags(posts[0].node.html)
+                            .split(' ')
+                            .slice(0, index === 0 ? this.state.textLines : 40)
+                            .join(' ')} ...`}
+                        </p>
+                      )}
+                      <Link
+                        className={`read-more`}
+                        key={post.node.id}
+                        to={post.node.frontmatter.path}
+                      >
+                        Read Post
+                      </Link>
+                    </div>
+                  </React.Fragment>
+                )}
+                {index > 4 && (
+                  <React.Fragment>
+                    {post.node.frontmatter.image && (
+                      <img
+                        className="post-image"
+                        src={post.node.frontmatter.image}
+                        alt=""
+                      />
+                    )}
+                    <div className="post-content">
+                      <header className="post-header">
+                        {index <= 0 ? (
+                          <h1
+                            className={classnames(
+                              'post-title',
+                              {
+                                'bodoni-24': index === 0
+                              },
+                              {
+                                'freight-sans': index > 0
+                              }
+                            )}
+                          >
+                            {post.node.frontmatter.title}
+                          </h1>
+                        ) : (
+                          <h2 className="post-title">
+                            {post.node.frontmatter.title}
+                          </h2>
+                        )}
+                        <time
+                          className="post-date"
+                          dateTime={new Date(
+                            post.node.frontmatter.date
+                          ).toLocaleString('en-US', {
+                            timeZone: 'UTC'
+                          })}
+                        >
+                          {new Date(post.node.frontmatter.date).toLocaleString(
+                            'en-US',
+                            {
+                              timeZone: 'UTC'
+                            }
+                          )}
+                        </time>
+                      </header>
+                      {post.node.frontmatter.introduction && (
+                        <p className="post-excerpt">
+                          {`${striptags(posts[0].node.html)
+                            .split(' ')
+                            .slice(0, index === 0 ? this.state.textLines : 40)
+                            .join(' ')} ...`}
+                        </p>
+                      )}
+                      <Link
+                        className={`read-more`}
+                        key={post.node.id}
+                        to={post.node.frontmatter.path}
+                      >
+                        Read Post
+                      </Link>
+                    </div>
+                  </React.Fragment>
+                )}
               </MediaQuery>
-              <MediaQuery query="(max-device-width: 1024px)">
+              <MediaQuery query="(max-device-width: 1023px)">
                 <header className="post-header">
                   {index <= 0 ? (
                     <h1 className="post-title">
