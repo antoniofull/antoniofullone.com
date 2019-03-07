5741442299d30fd50cf02cf600063f5473e02b21
Antonio <antonio.fullone@gmail.com>
2019-03-07 18:24:19 +0100

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
