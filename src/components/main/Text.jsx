e5b1e345906c22ad996ed872378d20da68ad43e4
antoniofull <antonio.fullone@gmail.com>
2019-10-20 19:31:41 +0200

    Implement react-fluid-text

--- src/components/main/Text.jsx
+++ src/components/main/Text.jsx
@@ -1,91 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import FluidText from 'react-fluid-text';
 
-import Observable from '../Observable';
 import { ThemeConsumer } from '../ThemeContext';
 
-class Text extends Component {
-  constructor(props) {
-    super(props);
-    this.container = React.createRef();
-    this.state = {};
-    this.updateSize = this.updateSize.bind(this);
-    this.textElement = React.createRef();
-  }
+const Text = props => {
+  const { isLoading } = props;
 
-  componentDidMount() {
-    this.updateSize();
-    if (typeof window !== 'undefined') {
-      window.addEventListener('resize', this.updateSize);
-    }
-  }
-
-  componentWillUnmount() {
-    if (typeof window !== 'undefined') {
-      window.removeEventListener('resize', this.updateDimensions);
-    }
-  }
-
-  updateSize() {
-    const { viewport } = this.props;
-
-    if (this.container.current) {
-      const parentWidth = this.container.current.clientWidth;
-      const minFontSize = Number.NEGATIVE_INFINITY;
-      const maxFontSize = Number.POSITIVE_INFINITY;
-      const comp = viewport < 600 ? 0.85 : 0.77;
-      const fontSize = Math.max(
-        Math.min(parentWidth / (comp * 10), parseFloat(maxFontSize)),
-        parseFloat(minFontSize)
-      );
-      this.setState({
-        fontSize: `${fontSize}px`
-      });
-    }
-  }
-
-  render() {
-    const { fontSize } = this.state;
-    const { animateElement, isLoading } = this.props;
-    return (
-      <div className="intro container" ref={this.container}>
-        <Observable
-          element="h1"
-          callback={animateElement}
-          className={classnames('intro__header', 'text-center', 'bodoni-24', {
+  return (
+    <div className="intro container">
+      <FluidText
+        className={classnames('intro__header', 'text-center', 'bodoni-24', {
+          'is-loaded': !isLoading
+        })}
+        compressor={0.8}
+        text="Antonio Fullone"
+      />
+      <div className="tagline">
+        <h2
+          className={classnames('intro__tagline', 'text-center', 'bodoni-24', {
             'is-loaded': !isLoading
           })}
-          ref={this.textElement}
-          data-theme="primary-light"
-          style={fontSize ? { fontSize } : null}
         >
-          Antonio Fullone
-        </Observable>
-        <div className="tagline">
-          <h2
-            className={classnames(
-              'intro__tagline',
-              'text-center',
-              'bodoni-24',
-              { 'is-loaded': !isLoading }
-            )}
-          >
-            Designer &amp; Developer
-          </h2>
-          <span className="i-am-cool text-center bodoni-24">Wannabe Cook</span>
-        </div>
-        <span className="intro__qualified text-center bodoni-24">
-          {'- Vespa Driver - '}
-        </span>
+          Designer &amp; Developer
+        </h2>
+        <span className="i-am-cool text-center bodoni-24">Wannabe Cook</span>
       </div>
-    );
-  }
-}
+      <span className="intro__qualified text-center bodoni-24">
+        {'- Vespa Driver - '}
+      </span>
+    </div>
+  );
+};
 
 Text.propTypes = {
-  viewport: PropTypes.number.isRequired,
-  animateElement: PropTypes.func.isRequired
+  isLoading: PropTypes.bool.isRequired
 };
 
 const TextContainer = () => (
